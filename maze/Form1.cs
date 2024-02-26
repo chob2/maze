3b27c01f20f9b2d2b49dcf6f0bc7d8261be80cd2
ryan.cheema <ryan.cheema@PHCS-034195>
2024-02-26 14:08:37 +1100

    Add project files.

---
+++ maze/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace maze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void makeCells()
+        {
+            int gridSizeX = 5;
+            int gridSizeY = 5;
+
+            Panel[,] cellCntr = new Panel[gridSizeX, gridSizeY];
+
+            PictureBox[,] wallN = new PictureBox[gridSizeX, gridSizeY]; //top wall
+            PictureBox[,] wallE = new PictureBox[gridSizeX, gridSizeY]; //right wall
+            PictureBox[,] wallS = new PictureBox[gridSizeX, gridSizeY]; //bottom wall
+            PictureBox[,] wallW = new PictureBox[gridSizeX, gridSizeY]; //left wall
+
+            for (int i = 0; i < gridSizeY; i++)
+            {
+                for (int j = 0; j < gridSizeX; j++)
+                {
+                    var newPanel = new Panel();
+                    {
+                        newPanel.Location = new Point(100 * j, 100 * i);
+                        newPanel.Size = new Size(100, 100);
+                        newPanel.BackColor = Color.Transparent;
+                        newPanel.BorderStyle = BorderStyle.FixedSingle;
+                    }
+
+                    Controls.Add(newPanel);
+                    cellCntr[i, j] = newPanel;
+
+
+                }
+            }
+            for (int i = 0; i < gridSizeY; i++)
+            {
+                for (int j = 0; j < gridSizeX; j++)
+                {
+                    var pictureBox = new PictureBox();
+                    {
+                        // pictureBox.Location = new Point(cellCntr[i, j].Location.X, cellCntr[i, j].Location.Y);
+                        pictureBox.Location = new Point(600,600);
+                        pictureBox.Size = new Size(100, 20);
+                        pictureBox.BackColor = Color.Black;
+                    }
+                    Controls.Add(pictureBox);
+                    wallN[i, j] = pictureBox;
+                    cellCntr[i, j].Controls.Add(wallN);
+
+
+                }
+            }
+        }
+        private void drawMaze()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            makeCells();
+        }
+    }
+}
---
+++ maze/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("maze")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pacific Hills Christian Education")]
+[assembly: AssemblyProduct("maze")]
+[assembly: AssemblyCopyright("Copyright © Pacific Hills Christian Education 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a3c88f5-5764-4916-822a-9f740697b5c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
