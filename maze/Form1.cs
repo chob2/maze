ad0b0db56cbf05ffc0813a3decb5d2bfe206f01a
Ryan Cheema <ryancheema122@gmail.com>
2024-03-23 17:23:36 +1100

    I DID IT!!!!!! I AM THE CODER!!!!! maze now generates without issue, also scalable by changing gridsize

--- maze/Form1.cs
+++ maze/Form1.cs
@@ -11,7 +11,7 @@ namespace maze
         {
             InitializeComponent();
         }
-        public static int gridSize = 5;
+        public static int gridSize = 9;
         public static int gridCount = gridSize * gridSize;
         class cell
         {
@@ -42,8 +42,8 @@ namespace maze
                 {
                     var newPanel = new Panel();//making panel
                     {
-                        newPanel.Location = new Point(100 * j, 100 * i);
-                        newPanel.Size = new Size(100, 100);
+                        newPanel.Location = new Point(50 * j, 50 * i);
+                        newPanel.Size = new Size(50, 50);
                         newPanel.BackColor = Color.Transparent;
 
                     }
@@ -61,7 +61,7 @@ namespace maze
                     var pictureBox = new PictureBox();
                     {
                         pictureBox.Location = new Point(0, 0);
-                        pictureBox.Size = new Size(100, 2);
+                        pictureBox.Size = new Size(50, 2);
                         pictureBox.BackColor = Color.Black;
                     }
                     wallN[i, j] = pictureBox;
@@ -77,8 +77,8 @@ namespace maze
                 {
                     var pictureBox = new PictureBox();
                     {
-                        pictureBox.Location = new Point(98, 0);
-                        pictureBox.Size = new Size(2, 100);
+                        pictureBox.Location = new Point(48, 0);
+                        pictureBox.Size = new Size(2, 50);
                         pictureBox.BackColor = Color.Black;
                     }
                     wallE[i, j] = pictureBox;
@@ -94,8 +94,8 @@ namespace maze
                 {
                     var pictureBox = new PictureBox();
                     {
-                        pictureBox.Location = new Point(0, 98);
-                        pictureBox.Size = new Size(100, 2);
+                        pictureBox.Location = new Point(0, 48);
+                        pictureBox.Size = new Size(50, 2);
                         pictureBox.BackColor = Color.Black;
                     }
                     wallS[i, j] = pictureBox;
@@ -112,7 +112,7 @@ namespace maze
                     var pictureBox = new PictureBox();
                     {
                         pictureBox.Location = new Point(0, 0);
-                        pictureBox.Size = new Size(2, 100);
+                        pictureBox.Size = new Size(2, 50);
                         pictureBox.BackColor = Color.Black;
                     }
                     wallW[i, j] = pictureBox;
@@ -125,90 +125,15 @@ namespace maze
 
 
         private void drawMaze()
-        {
-            /* cell[,] cells = new cell[gridSize, gridSize]; //cell class
-
-
-             Random rand = new Random();
-
-
-
-
-
-
-
-
-             for (int i = 0; i < (gridSize); i++) //adding cells class to grid
-             {
-                 for (int j = 0; j < gridSize; j++)
-                 {
-                     cell newCell = new cell();
-                     cells[i, j] = newCell;
-                 }
-             }
-
-            // cells[x, y].colony = true;
-             int tag = 0; //assigns colony cell to an index
-
-             cell[,] poolCells = new cell[gridSize, gridSize];
-             cell newPoolCell = new cell();
-
-             int x = rand.Next(0, gridSize); //x = rows
-             int y = rand.Next(0, gridSize);//y = columns
-
-             cells[x, y].tag = tag; //first cell index at 0
-             tag++;
-
-             int[] neighbourRowOffsets = { -1, 0, 1, 0 };
-             int[] neighbourColOffsets = { 0, 1, 0, -1 };
-             for (int direction = 0; direction < 4; direction++)
-             {
-                 int newRow = x + neighbourRowOffsets[direction];
-                 int newCol = y + neighbourColOffsets[direction];
-
-                 if(newRow >=0 && newRow < gridSize && newCol >= 0 && newCol < gridSize)
-                 {
-                     poolCells[newRow, newCol] = newPoolCell;
-                 }
-             }
-
-             for (int i = 0; i < gridSize * gridSize; i++)
-             {   
-
-
-
-
-
-
-                 for (int direction = 0; direction < 4; direction++)
-                 {
-
-                 }
-             }*/
-
-
-
-            cell[,] cells = new cell[gridSize, gridSize]; //cell class
-
-            for (int I = 0; I < (gridSize); I++) //adding grids to a class so i can interact with them
-            {                                       //i = row, j = column
-                for (int J = 0; J < gridSize; J++)
-                {
-                    cell newCell = new cell();
-                    cells[I, J] = newCell;
-                }
-            }
-
-
-
-
+        {           
             Random rand = new Random();
 
             int i = rand.Next(0, gridSize);//randomly choosing a cell 
             int j = rand.Next(0, gridSize); //i = rows and j = columns
+                                            //i = Y value of point, j = x value of point
 
             List<Point> chosenCells = new List<Point>();//adding starting cell to chosen cells list
-            Point chosenPoint = new Point(i, j);
+            Point chosenPoint = new Point(j, i);
             chosenCells.Add(chosenPoint);
 
             List<Point> potentialNext = new List<Point>();//this will be the list of cells adjacent to chosen cells that may be chosen next iteration
@@ -220,50 +145,84 @@ namespace maze
             {                                                   //allows me to add to potentialNext without creating runtime error
                 int newRow = i + neighbourRowOffsets[direction];
                 int newCol = j + neighbourColOffsets[direction];
-                Point nextPoint = new Point(newRow,newCol);
+                Point nextPoint = new Point(newCol,newRow);
 
                 if (newRow >= 0 && newRow < gridSize && newCol >= 0 && newCol < gridSize)
                 {
                     potentialNext.Add(nextPoint); //adds cell to list if it is inside the grid
                 }
             }
+
             int count = 0;
-            while (count < gridCount - 1)
+            while (count < (gridCount-1))
             {
                 int r = rand.Next(0, potentialNext.Count);
-                i = potentialNext[r].X; //indexing a random potential cell's coordinates
-                j = potentialNext[r].Y;
+                i = potentialNext[r].Y; //indexing a random potential cell's coordinates
+                j = potentialNext[r].X;
 
                 int index = 0;
                 Point[] neighbours = new Point[4];//potential neighbouring chosen points
-                int[] dirI = new int[4];
-                int[] dirJ = new int[4];
+
                 for (int direction = 0; direction < 4; direction++) //checking neighbouring cells for a chosen cell
                 {
                     int newRow = i + neighbourRowOffsets[direction];
                     int newCol = j + neighbourColOffsets[direction];
 
-                    Point nextPoint = new Point(newRow, newCol);//selected point from potentialNext
+                    Point nextPoint = new Point(newCol, newRow);//selected point from potentialNext
 
                     if (newRow >= 0 && newRow < gridSize && newCol >= 0 && newCol < gridSize)
                     {
                         if (chosenCells.Contains(nextPoint))
                         {
                             neighbours[index] = nextPoint;
-                            dirI[index] = neighbourRowOffsets[direction];
-                            dirJ[index] = neighbourColOffsets[direction];
                             index++;
                         }
                     }
                 }
                 
+
+
+
+                int targ = rand.Next(0, index);
+                //if neighbours[targ].X > potentialNext[r].X -> target cell is to the right
+                if(neighbours[targ].X > potentialNext[r].X)
+                {
+                    wallE[potentialNext[r].Y, potentialNext[r].X].Visible = false;
+                    wallW[neighbours[targ].Y, neighbours[targ].X].Visible = false;
+                }
+                //if neighbours[targ].X < potentialNext[r].X -> target cell is to the left
+                else if (neighbours[targ].X < potentialNext[r].X)
+                {
+                    wallW[potentialNext[r].Y, potentialNext[r].X].Visible = false;
+                    wallE[neighbours[targ].Y, neighbours[targ].X].Visible = false;
+                }
+                //if neighbours[targ].Y < potentialNext[r].Y -> target cell is above
+                else if (neighbours[targ].Y < potentialNext[r].Y)
+                {
+                    wallN[potentialNext[r].Y, potentialNext[r].X].Visible = false;
+                    wallS[neighbours[targ].Y, neighbours[targ].X].Visible = false;
+                }
+                //if neighbours[targ].Y > potentialNext[r].Y -> target cell is below
+                else if(neighbours[targ].Y > potentialNext[r].Y)
+                {
+                    wallS[potentialNext[r].Y, potentialNext[r].X].Visible = false;
+                    wallN[neighbours[targ].Y, neighbours[targ].X].Visible = false;
+                }
+                //if target cell is above, remove potentialNext[targ] north wall and remove target cell south wall
+                
+
+
+
+
+
+
                 //find neighbours of potentialNext[r]
                 //if neighbours arent chosen or in potentialNext, add to potentialNext
                 for (int direction = 0; direction < 4; direction++) //this checks for the cells adjacent to the chosen cell
                 {                                                   //allows me to add to potentialNext without creating runtime error
                     int newRow = i + neighbourRowOffsets[direction];
                     int newCol = j + neighbourColOffsets[direction];
-                    Point nextPoint = new Point(newRow, newCol);
+                    Point nextPoint = new Point(newCol, newRow);
 
                     if (newRow >= 0 && newRow < gridSize && newCol >= 0 && newCol < gridSize)
                     {
@@ -273,31 +232,6 @@ namespace maze
 
                         }
                     }
-                }
-                int targ = rand.Next(0, index);
-                //if target cell is above, remove potentialNext[targ] north wall and remove target cell south wall
-                if (dirI[targ] == 1 && dirJ[targ] == 0)
-                {
-                    wallN[i, j].Visible = false;
-                    wallS[i + dirI[targ], j + dirJ[targ]].Visible = false;
-                }
-                //if target cell is below, remove potentialNext[targ] south wall and remove target cell north wall
-                else if (dirI[targ] == 1 && dirJ[targ] == 0)
-                {
-                    wallS[i, j].Visible = false;
-                    wallN[i + dirI[targ], j + dirJ[targ]].Visible = false;
-                }
-                //if target cell is to the right, remove potentialNext[targ] east wall and remove target cell west wall
-                else if (dirI[targ] == 1 && dirJ[targ] == 0)
-                {
-                    wallE[i, j].Visible = false;
-                    wallW[i + dirI[targ], j + dirJ[targ]].Visible = false;
-                }
-                //if target cell is to the left, remove potentialNext[targ] west wall and remove target cell east wall
-                else if (dirI[targ] == 1 && dirJ[targ] == 0)
-                {
-                    wallW[i, j].Visible = false;
-                    wallE[i + dirI[targ], j + dirJ[targ]].Visible = false;
                 }
 
                 chosenCells.Add(potentialNext[r]);//these two lines are what causes runtime error in line 234
